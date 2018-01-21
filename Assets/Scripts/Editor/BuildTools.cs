4db97fe88594af59c36af795443cfbf3f86187d2
svekr <sviokr@rambler.ru>
2018-01-21 22:11:50 +0200

    asset-bundles. Task #8.

---
+++ Assets/Scripts/Editor/BuildTools.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Diagnostics;
+
+public class BuildTools {
+
+    [MenuItem("Tools/Build Asset Bundles Standalone")]
+    public static void BuildAssetBundlesStandalone() {
+        BuildAssetBundlesMethod(BuildTarget.StandaloneWindows);
+    }
+
+    [MenuItem("Tools/Build Asset Bundles Android")]
+    public static void BuildAssetBundlesAndroid() {
+        BuildAssetBundlesMethod(BuildTarget.Android);
+    }
+
+    [MenuItem("Tools/Build Asset Bundles iOS")]
+    public static void BuildAssetBundlesIOS() {
+        BuildAssetBundlesMethod(BuildTarget.iOS);
+    }
+
+    [MenuItem("Tools/Make Build Standalone")]
+    public static void MakeBuildWindows() {
+        string fileName = MakeBuild(BuildTarget.StandaloneWindows, "exe");
+        RunBuild(fileName);
+    }
+
+    [MenuItem("Tools/Make Build Android")]
+    public static void MakeBuildAndroid() {
+        MakeBuild(BuildTarget.Android, "apk");
+    }
+
+    [MenuItem("Tools/Make Build iOS")]
+    public static void MakeBuildIOS() {
+        MakeBuild(BuildTarget.iOS, "ipa");
+    }
+
+    private static void BuildAssetBundlesMethod(BuildTarget buildTarget) {
+        string directory = Path.Combine(Application.streamingAssetsPath, "assetBundles");
+        if (!Directory.Exists(directory)) {
+            Directory.CreateDirectory(directory);
+        }
+        BuildPipeline.BuildAssetBundles(directory, BuildAssetBundleOptions.None, buildTarget);
+    }
+
+    private static string MakeBuild(BuildTarget buildTarget, string extension) {
+        string fileName = null;
+        if (!BuildPipeline.isBuildingPlayer) {
+            string directory = Path.Combine(Application.persistentDataPath, "/builds");
+            fileName = EditorUtility.SaveFilePanel("Select build name", directory, "build", extension);
+            if (fileName != null && fileName.Length > 0) {
+                BuildPlayerOptions options = new BuildPlayerOptions();
+                options.scenes = new[] {"Assets/Scenes/Scene.unity"};
+                options.locationPathName = fileName;
+                options.target = buildTarget;
+                options.options = BuildOptions.None;
+                BuildPipeline.BuildPlayer(options);
+            }
+        }
+        return fileName;
+    }
+
+    private static void RunBuild(string fileName) {
+        if (fileName != null && fileName.Length > 0) {
+            Process process = new Process();
+            process.StartInfo.FileName = fileName;
+            process.Start();
+        }
+    }
+}
---
+++ Assets/Scripts/LoadAssetBundle.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class LoadAssetBundle : MonoBehaviour {
+
+    void Start() {
+        //LoadBundleFromFile();
+        StartCoroutine(LoadBundleWithRequest());
+    }
+
+    private IEnumerator LoadBundleWithRequest() {
+#if UNITY_EDITOR
+        string path = "file:///" + Path.Combine(Application.streamingAssetsPath, "assetBundles/prototyping/blocks");
+#elif UNITY_ANDROID
+        string path = "jar:file:///" + Application.dataPath + "!/assets/" + "assetBundles/prototyping/blocks";
+#else
+        string path = "file:///" + Application.dataPath + "/Raw/" + "assetBundles/prototyping/blocks";
+#endif
+        Debug.Log("Start load bundle: " + path);
+        UnityWebRequest request = UnityWebRequest.GetAssetBundle(path);
+        yield return request.SendWebRequest();
+
+        Debug.Log("Load complete.");
+        AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(request);
+        ProcessAssetBundle(bundle);
+    }
+
+    private void LoadBundleFromFile() {
+        string path = Path.Combine(Application.streamingAssetsPath, "assetBundles/prototyping/blocks");
+        AssetBundle bundle = AssetBundle.LoadFromFile(path);
+        ProcessAssetBundle(bundle);
+    }
+
+    private void ProcessAssetBundle(AssetBundle bundle) {
+        if (bundle == null) {
+            Debug.Log("Asset bundle load error.");
+        } else {
+            Debug.Log("Asset bundle load complete successfully.");
+            ProcessPrefab(bundle, "CubeGray", Vector3.zero);
+            ProcessPrefab(bundle, "CubePink", new Vector3(10f, 0f, -10f));
+        }
+    }
+
+        private void ProcessPrefab(AssetBundle bundle, string prefabName, Vector3 position) {
+        GameObject prefab = bundle.LoadAsset<GameObject>(prefabName);
+        if (prefab == null) {
+            Debug.Log("Prefab load error.");
+        } else {
+            Debug.Log("Prefab loaded successfully.");
+            Instantiate(prefab, position, Quaternion.identity);
+        }
+    }
+}
